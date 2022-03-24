3ea440844f378260b9dc90eb60a6db66a0b40307
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-03-24 11:59:20 -0300

    feat: add event model

---
+++ aws-project-01/src/main/java/br/com/siecola/model/Envelope.java
@@ -0,0 +1,23 @@
+package br.com.siecola.model;
+
+public class Envelope {
+
+    private String eventType;
+    private String data;
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
