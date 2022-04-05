8ab9175dd730e0a40b2d15f1a770408e21008a3d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-04-04 22:33:47 -0300

    build: add Dockerfile configuration

--- aws-project-02/src/main/java/br/com/siecola/AwsProject02Application.java
+++ aws-project-02/src/main/java/br/com/siecola/AwsProject02Application.java
@@ -1,4 +1,4 @@
-package br.com.siecola.awsproject02;
+package br.com.siecola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
