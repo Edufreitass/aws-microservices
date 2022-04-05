ec2a78c45899f4ad884bea03dc9777b4f59614ea
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-04-04 22:12:26 -0300

    feat: create new project

---
+++ aws-project-02/src/test/java/br/com/siecola/awsproject02/AwsProject02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.siecola.awsproject02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProject02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
